e896938263ea63667ba3706f51ca8df323cd6758
Cornel Rantes <cornel.rantes@justbe.co.uk>
2020-10-17 12:49:35 +0300

    Add TestNG and tests

---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,10 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class SecondTest {
+
+    @Test
+    public void secondTest() {
+        Assert.assertNotEquals(1, 2);
+    }
+}
